a0e9b4b437e93ca34be4b8545f380cbb836339be
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-04-10 11:53:06 +0900

    Update this.js

--- lecture/this.js
+++ lecture/this.js
@@ -1,9 +1,20 @@
-console.log(this); //global?
-console.log(this === module.exports); //exports === module.exports === {}
+// console.log(this); //global?
+// console.log(this === module.exports); //exports === module.exports === {}
 
-//function 안의 this 만 global에 해당함!
-function a() {
-  console.log(this === global);
+// //function 안의 this 만 global에 해당함!
+// function a() {
+//   console.log(this === global);
+// }
+
+// a();
+
+//자바스크립트의 함수는 호출될 때, 매개 변수로 전달되는 인자값 이외에,
+//arguments 객체와 this를 암묵적으로 전달 받는다.
+function sqare(num) {
+  console.log("arguments: ", arguments);
+  console.log("this: ", this);
+
+  return num * num;
 }
 
-a();
+console.log(sqare(2));
