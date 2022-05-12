54d9365d25c1ba9fcc3f07b825f6d97b56a53ef1
jason taekyun Kim <saurus234@gmail.com>
2022-05-12 11:43:22 +0900

    cookie, session, https, http2

---
+++ ch4/4.4/server-3.js
@@ -0,0 +1,23 @@
+const https = require("https");
+const fs = require("fs");
+
+https
+  .createServer(
+    {
+      //readFileSync => Sync써도 되는 경우 : 딱한번만 실행하거나, 서버의 경우 시작하기 전 초기화 할때!
+      cert: fs.readFileSync("도메인 인증서 경로"),
+      key: fs.readFileSync("도메인 비밀키 경로"),
+      ca: [
+        fs.readFileSync("상위 인증서 경로"),
+        fs.readFileSync("상위 인증서 경로"),
+      ],
+    },
+    (req, res) => {
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.write("<h1>Hello Node!</h1>");
+      res.end("<p>Hello Server!</p>");
+    }
+  )
+  .listen(443, () => {
+    console.log("443번 포트에서 서버 대기 중입니다!");
+  });
