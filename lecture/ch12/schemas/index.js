8cab611dbdba5958d5d31bcc5debb6bc93651adf
jason taekyun Kim <saurus234@gmail.com>
2022-06-06 00:28:19 +0900

    Update index.js

--- lecture/ch12/schemas/index.js
+++ lecture/ch12/schemas/index.js
@@ -30,7 +30,7 @@ const connect = () => {
 };
 
 mongoose.connection.on("error", (error) => {
-  console.error("몽고디비 연결 에러", error);
+  console.error("몽고디비 연결  에러", error);
 });
 mongoose.connection.on("disconnected", () => {
   console.error("몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.");
